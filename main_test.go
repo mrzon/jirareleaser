9651acf44ccfe0e74e90f7e816203b772c15ca0d
Emerson Simbolon <emerson@traveloka.com>
2024-02-07 15:20:40 +0700

    Add unit test and improve credit

--- main_test.go
+++ main_test.go
@@ -48,6 +48,38 @@ func Test_parseJiraReleaserData(t *testing.T) {
 				date:     "YYY",
 			},
 		},
+		{
+			"Project flag without project name",
+			[]string{"", "--p"},
+			&JiraConfig{Project: "ABC"},
+			nil,
+		},
+		{
+			"Invalid date format",
+			[]string{"", "31-12-2022", "Service1", "Service2"},
+			&JiraConfig{Project: "ABC"},
+			&jiraReleaserData{
+				config:   &JiraConfig{Project: "ABC"},
+				services: []string{"Service1", "Service2"},
+				date:     "31-12-2022",
+			},
+		},
+		{
+			"No services provided",
+			[]string{"", "2022-12-31"},
+			&JiraConfig{Project: "ABC"},
+			nil,
+		},
+		{
+			"Invalid project flag",
+			[]string{"", "-p", "STL", "2022-12-31", "Service1", "Service2"},
+			&JiraConfig{Project: "STL"},
+			&jiraReleaserData{
+				config:   &JiraConfig{Project: "STL"},
+				services: []string{"STL", "2022-12-31", "Service1", "Service2"},
+				date:     "-p",
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
